1fea2bef2b7ffea5a78e931978ed63cd48c9cdea
Clay <clayandrews13@gmail.com>
2016-09-26 22:07:36 +0000

    added autoincrement package

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var port = process.env.PORT || 8080;
 var mongo = require('mongodb');
 var dburl = process.env.MONGOLAB_URI;
 var express = require('express');
+var autoIncrement = require("mongodb-autoincrement");
 var buildUrl = require('./helpers/buildUrl.js');
 var checkUrl = require('./helpers/checkUrl.js');
 var app = express();
@@ -15,25 +16,23 @@ app.get('/', function(req,res) {
 //ADD A NEW SHORT URL//
 app.get('/new/*', function(req, res) {
     var url = req.params[0];
+    var count;
     if(checkUrl(url)) {
         mongo.MongoClient.connect(dburl, function(err, db) {
             if(err) {console.error('mongodb didnt connect')}
-            var urls = db.collection('urls');
-            function getNextSequenceValue(sequenceName){
-                var sequenceDocument = db.collection('counters').findAndModify({
-                    query:{_id: sequenceName },
-                    update: {$inc:{sequence_value:1}},
-                    new:true
-                });
-                return sequenceDocument.sequence_value;
-            }
-            urls.insert({_id: getNextSequenceValue('urlid'), url: url}, function(err, data) {
-                if(err) {console.error('insert didnt work')}
-                res.end(JSON.stringify(data));
-            });
-            db.close();
-        });
+            
+           autoIncrement.getNextSequence(db, 'urls', function (err, autoIndex) {
+               if(err) console.err(err);
+        var collection = db.collection('urls');
+        collection.insert({
+            _id: autoIndex,
+            url: url
+        }, function(err, data) { if(err) {console.error(err)} res.end(data.ops[0].url + ' saved as ' + data.ops[0]._id)});
+        db.close();
+           });
+    }); 
         
+            
     } else {
         res.end('invalid url');
     }
@@ -44,6 +43,7 @@ app.get('/*', function(req, res) {
     console.log('2nd get');
     var redirect = '';
     var hash = req.params[0];
+    console.log(hash);
    mongo.MongoClient.connect(dburl, function(err, db) {
        if(err) console.log('unable to connect to mongodb server: ' + err);
        var urls = db.collection('urls');
@@ -53,14 +53,10 @@ app.get('/*', function(req, res) {
        //IT EITHER NEEDS TO FIND THE AUTOMATICALLY ASSIGNED OBJECT ID, OR WE NEED TO ASSIGN OUR OWN ID AND FIND THAT
        
        /////////////////////////
-       urls.find({_id: hash}).toArray(function(err, documents){ // this is where you'd want to hash the id to make it shorter
+       urls.find({_id:parseInt(hash)}).toArray(function(err, documents){ // this is where you'd want to hash the id to make it shorter
            if(err) throw err;
-           if(documents[0]) {
-            redirect = documents[0].url;
-           } else {
-               redirect = 'not found';
-           }
-           console.log(redirect);
+           console.log(documents[0].url);
+           redirect = documents[0].url;
        });
        db.close();
    });
